db6b899389f246cb6d7f46f92be1add0032df08c
Pavel Konyaev <konyaev.p@gmail.com>
2022-02-20 15:04:43 +0300

    [task] fixes

--- solution.go
+++ solution.go
@@ -8,6 +8,8 @@ const (
 	SidesCircle   int64 = 0
 )
 
+type Sides int64
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -16,7 +18,7 @@ const (
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum int64) float64 {
+func CalcSquare(sideLen float64, sidesNum Sides) float64 {
 	if sidesNum == SidesCircle {
 		return math.Pi * math.Pow(sideLen, 2)
 	}
